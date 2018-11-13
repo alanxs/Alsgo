c2ba2382b090bf510e740706e0777a2fffa2232a
alan <346579640@qq.com>
2018-11-13 15:40:08 +0800

    INIT

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alsgo
+//
+//  Created by alan on 2018/11/13.
+//  Copyright © 2018 alsgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
